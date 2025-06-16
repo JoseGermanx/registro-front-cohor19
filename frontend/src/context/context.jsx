4557a723df5ed5a4b89f2931517dbf53c0931108
José G. Martínez <jgermanmm@gmail.com>
2025-06-16 10:32:52 -0400

    Eliminar comentario innecesario sobre prop-types en AuthProvider

--- frontend/src/context/context.jsx
+++ frontend/src/context/context.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext();
 
-// eslint-disable-next-line react/prop-types
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
